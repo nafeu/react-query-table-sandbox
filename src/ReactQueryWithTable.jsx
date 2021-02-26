5f5b69383d5fa590c5f7443bc2685355ae8ed25c
Nafeu Nasir <nafeu@tenjin.com>
2021-02-26 11:41:16 -0500

    Add placeholder component for ReactQueryWithTable

---
+++ src/ReactQueryWithTable.jsx
@@ -0,0 +1,11 @@
+import React from 'https://cdn.skypack.dev/react';
+
+const ReactQueryWithTable = () => {
+  return (
+    <div>
+      [CONTENT]
+    </div>
+  );
+}
+
+export default ReactQueryWithTable;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,32 @@
 import React from 'https://cdn.skypack.dev/react';
 import ReactDOM from 'https://cdn.skypack.dev/react-dom';
+import './index.css';
+
+import ReactQueryWithTable from './ReactQueryWithTable.jsx';
+
+const Section = ({ title, desc, children }) => {
+  return (
+    <React.Fragment>
+      <div className="section-title">{title}</div>
+      <div className="section-desc">{desc}</div>
+      <div className="section-content">
+        {children}
+      </div>
+    </React.Fragment>
+  );
+}
 
 const App = () => {
   return (
-    <div>
-      Hello World.
+    <div class="app">
+      <div class="app-container">
+        <Section
+          title={'React Query with React Table'}
+          desc={'Using React Query with React Table'}
+        >
+          <ReactQueryWithTable />
+        </Section>
+      </div>
     </div>
   );
 }
