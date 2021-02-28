aa9e222f1190722eb61f8a24e2dfaf9a81e3fbcf
Nafeu Nasir <nafeu@tenjin.com>
2021-02-28 17:17:18 -0500

    Add routing and fix repeat queries in expanding rows

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {
+  HashRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 import './index.css';
 
 import ReactQueryWithTable from './ReactQueryWithTable.jsx';
@@ -21,21 +27,42 @@ const App = () => {
   return (
     <div className="app">
       <div className="app-container">
-        <Section
-          title={'React Query with React Table'}
-          desc={'Using React Query with React Table'}
-        >
-          <ReactQueryWithTable />
-        </Section>
-        <Section
-          title={'React Table with Expanded Rows'}
-          desc={'Using and API request to lazy-load expanded rows'}
-        >
-          <ReactTableExpanding />
-        </Section>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">React Query with React Table</Link>
+            </li>
+            <li>
+              <Link to="/expanding">React Table with Expanding Rows</Link>
+            </li>
+          </ul>
+        </nav>
+        <Switch>
+          <Route exact path="/">
+            <Section
+              title={'React Query with React Table'}
+              desc={'Using React Query with React Table'}
+            >
+              <ReactQueryWithTable />
+            </Section>
+          </Route>
+          <Route path="/expanding">
+            <Section
+              title={'React Table with Expanded Rows'}
+              desc={'Using and API request to lazy-load expanded rows'}
+            >
+              <ReactTableExpanding />
+            </Section>
+          </Route>
+        </Switch>
       </div>
     </div>
   );
 }
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+  <Router>
+    <App />
+  </Router>,
+  document.getElementById("root")
+);
